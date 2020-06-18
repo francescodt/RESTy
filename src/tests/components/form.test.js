6dde461a2bd787b04eec5c4fc5f1f42df9adc806
francescodt <francescodtroiano@gmail.com>
2020-06-17 22:25:03 -0500

    added testing

--- src/tests/components/form.test.js
+++ src/tests/components/form.test.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import Form from '../../components/form';
 
 describe('<Form />', () => {
-    it ('can render button', () => {
+    it('can render button', () => {
         let app = shallow(<Form />);
-        expect(app.find('button').text()).toBe('Works');
+        expect(app.find('button').text()).toBe('GO!');
+    });
+    
+    it('search box changes url state', () => {
+        let app = mount(<Form />);
+        let input = app.find('input');
+        input.simulate('change', { target: { value: 'abc' } });
+        expect(app.state('url')).toBe('abc');
+    });
+
+    it('method click changes method state', () => {
+        let app = mount(<Form />);
+        let input = app.find('span#get');
+        input.simulate('click');
+        expect(app.state('method')).toBe('get');
     });
 
 
